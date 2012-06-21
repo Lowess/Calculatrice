fdabc798ac2d61b470963adea4a6a7ef158b33c4
Florian Dambrine <android.florian@gmail.com>
2012-06-21 19:10:03 +0200

    Projet LO21 - DAMBRINE Florian & REANEY Olivia

--- src/LogSystem.cpp
+++ src/LogSystem.cpp
@@ -9,25 +9,12 @@ void LO21::LogSystem::ecrireConsole(const LogMessage& m){
 void LO21::LogSystem::ecrireFichier(const LogMessage& m){
     QFile file(QDir::currentPath() + "Calculatrice.log");
 
-    QString old;
-
-    //Lecture
-    try{
-        if(!file.open(QIODevice::ReadOnly | QIODevice::Text)){
-            throw CalculatriceException("LogSystem",OTHER,"Echec d'ouverture de fichier de log en lecture");
-        }
-    } catch (std::exception& e) {}
-
-    old=file.readAll();
-    file.close();
-
     //Ecriture
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)){
+    if (!file.open(QIODevice::Append | QIODevice::Text)){
         throw CalculatriceException("LogSystem",OTHER,"Echec d'ouverture de fichier de log en ecriture");
     }
     QTextStream out(&file);
-    out << old << "\n";
-    out << m.info();
+    out << "\n" << m.info();
 
     file.close();
 
